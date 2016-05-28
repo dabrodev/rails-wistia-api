35313c2c3deaea9518c6d8fd6d2ac9b0d010918b
Damian Dabrowski <dabdamian@gmail.com>
2016-05-28 15:43:45 +0200

    refactor: material design and wistia api

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160528100200) do
+ActiveRecord::Schema.define(version: 20160528103211) do
 
   create_table "videos", force: :cascade do |t|
     t.string   "wistia"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "title"
   end
 
 end
